231e34983cbfc7223084c93e8c5d8683cdfdc620
samikhailov <ko_0n@mail.ru>
2020-03-15 21:53:23 +0300

    Рефакторизовано представления домашней страницы для использования шаблона

--- lists/tests.py
+++ lists/tests.py
@@ -1,22 +1,9 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
-    def test_root_url_resolves_to_home_page(self):
-        """Тест: корневой url преобразуется в представление домашней страницы"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_corrext_html(self):
-        """Тест: домашняя страница возвращает правильный hhtml"""
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        """Тест: используется домашний шаблон"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
