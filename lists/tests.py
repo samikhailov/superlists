a73d51dca3941fbd2f2141bc8d722dc21f88359a
samikhailov <ko_0n@mail.ru>
2020-03-15 23:01:05 +0300

    ФТ пополнился доп. шагами. Добавлена функциональность для обработки POST-запроса

--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,9 @@ class HomePageTest(TestCase):
         """Тест: используется домашний шаблон"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """Можно сохранить POST-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
