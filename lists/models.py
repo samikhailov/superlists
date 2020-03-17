fac7e5af63f7eec79b7fb604bd6c94b352d8da8a
samikhailov <ko_0n@mail.ru>
2020-03-17 15:52:03 +0300

    Добавлена модель и миграции

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+
+class List(models.Model):
+    '''список'''
+    pass
+
 class Item(models.Model):
     '''элемент списка'''
     text = models.TextField(default='')
+    list = models.ForeignKey(List, null=True, default=None)
